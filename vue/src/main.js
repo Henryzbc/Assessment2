90c9e7bb930f7363d7d1890abae05c7b4a7a2d98
Henryzbc <3289435499@qq.com>
2024-10-09 09:53:52 +0800

    Plug-in optimization

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 import $ from 'jquery'
-import { loadStyle } from './util/util'
 import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
 Vue.use(ElementUI)
@@ -11,28 +10,16 @@ import './styles/common.scss';
 
 import App from './App'
 import router from './router/index'
-import store from './store';
 
 import '../static/js/jquery.min.js'
 
 import './styles/top.scss';
-
-Vue.use(window.AVUE, {
-  size: 'mini',
-  tableSize: 'mini',
-  calcHeight: 65,
-});
-
-Vue.config.productionTip = false
-
-loadStyle(`//at.alicdn.com/t/font_$key.css`.replace('$key', '567566_pwc3oottzol'));
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   $,
   router,
-  store,
   components: { App },
   template: '<App/>'
 })
